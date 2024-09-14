08c9509207d99d2268f90036385e1b609b220392
Cristian Sancricca <cristiansancricca@gmail.com>
2024-09-14 00:09:07 -0300

    Initial Commit

---
+++ CitiesSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CitiesSearchApp
+//
+//  Created by Cristian Sancricca on 14/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
